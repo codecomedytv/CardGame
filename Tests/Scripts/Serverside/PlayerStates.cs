102632a5d01fbf36f9a3163cba6d351fde2a7359
CodeDarigan <darigan@protonmail.com>
2020-05-28 11:15:47 +0100

    Finish Readding PlayerState Tests

--- Tests/Scripts/Serverside/PlayerStates.cs
+++ Tests/Scripts/Serverside/PlayerStates.cs
@@ -90,6 +90,51 @@ namespace CardGame.Tests.Scripts.Serverside
             Assert.IsEqual(oldState, Player.States.Active, "Player was Active");
             Assert.IsEqual(newState, Player.States.Passive, "Player is Passive");
         }
+
+        [Test]
+        public void NonTurn_Player_Becomes_Idle_When_TurnPlayer_Ends_Their_Turn()
+        {
+            StartGame(Decklist);
+            var oldState = Players[0].State;
+            Play.EndTurn(Players[1].Id);
+            var newState = Players[0].State;
+            
+            Assert.IsEqual(oldState, Player.States.Passive, "Player was Passive");
+            Assert.IsEqual(newState, Player.States.Idle, "Player is Idle");
+        }
+
+        [Test]
+        public void Passive_Opponent_Becomes_Active_When_IdlePlayer_Takes_Action()
+        {
+            StartGame(Decklist);
+            var oldState = Players[0].State;
+            var unit = Players[1].Hand[0].Id;
+            Play.Deploy(Players[1].Id, unit);
+            var newState = Players[0].State;
+            
+            Assert.IsEqual(oldState, Player.States.Passive, "Player was Passive");
+            Assert.IsEqual(newState, Player.States.Active, "Player is Active");
+        }
+
+        [Test]
+        public void ActivePlayer_Becomes_Active_When_They_Take_An_Action()
+        {
+            StartGame(Decklist);
+            var support = Players[1].Hand[3].Id;
+            Play.SetFaceDown(Players[1].Id, support);
+            Play.EndTurn(Players[1].Id);
+            var unit = Players[0].Hand[0].Id;
+            Play.Deploy(Players[0].Id, unit);
+            var oldState = Players[1].State;
+            Play.Activate(Players[1].Id, support, 0, new List<int>());
+            var newState = Players[1].State;
+            
+            Assert.IsEqual(oldState, Player.States.Active, "Player was Active");
+            Assert.IsEqual(newState, Player.States.Acting, "Player is Acting");
+
+        }
+        
+   
         
     }
 }
