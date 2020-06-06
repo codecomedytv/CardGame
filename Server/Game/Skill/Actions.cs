9deb07dd6886ae6c2ea6628635926a5a38bdc488
CodeDarigan <darigan@protonmail.com>
2020-06-06 10:55:33 +0100

    Split Skill Into Partial Class

---
+++ Server/Game/Skill/Actions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Godot.Collections;
+
+namespace CardGame.Server
+{
+    public partial class Skill
+    {
+        protected void AddTagToController(Tag tag)
+        {
+            var decorator = new Decorator(tag);
+            decorator.AddTagToPlayer(Controller);
+        }
+
+        protected void AddTagToGroup(List<Card> cards, Tag tag, string destroyCondition, string taggedExit, bool includeCard = true)
+        {
+            var decorator = new Decorator(tag);
+            if (destroyCondition != "")
+            {
+                Card.Connect(destroyCondition, decorator, nameof(decorator.OnZoneExit),
+                    new Array {Card.Zone, Card, "destroy"});
+            }
+
+            foreach (var card in cards)
+            {
+                if (Card == card && !includeCard)
+                {
+                    // I have no idea what this is doing?
+                    // Excluding card from its own tags
+                    continue;
+                }
+                decorator.AddTagTo(card);
+                if (taggedExit != "")
+                {
+                    card.Connect(taggedExit, decorator,
+                        nameof(decorator.OnZoneExit), new Array {card.Zone, card, "untag"});
+                }
+            }
+        }
+
+        protected void SetTargets(List<Card> cards)
+        {
+            Controller.DeclarePlay(new SetTargets(Card, cards));
+        }
+
+        protected void AutoTarget()
+        {
+            GameState.Paused = true;
+            Controller.DeclarePlay(new AutoTarget(Card));
+        }
+    }
+}
--- Server/Game/Skill/Skill.cs
+++ Server/Game/Skill/Skill.cs
@@ -7,7 +7,7 @@ using Object = Godot.Object;
 
 namespace CardGame.Server {
 	
-	public class Skill : Godot.Object, IResolvable
+	public partial class Skill : Godot.Object, IResolvable
 	{
 		public enum Types { Manual, Auto, Constant }
 
@@ -77,48 +77,7 @@ namespace CardGame.Server {
 		}
 
 
-		protected void AddTagToController(Tag tag)
-		{
-			var decorator = new Decorator(tag);
-			decorator.AddTagToPlayer(Controller);
-		}
-
-		protected void AddTagToGroup(List<Card> cards, Tag tag, string destroyCondition, string taggedExit, bool includeCard = true)
-		{
-			var decorator = new Decorator(tag);
-			if (destroyCondition != "")
-			{
-				Card.Connect(destroyCondition, decorator, nameof(decorator.OnZoneExit),
-					new Array {Card.Zone, Card, "destroy"});
-			}
-
-			foreach (var card in cards)
-			{
-				if (Card == card && !includeCard)
-				{
-					// I have no idea what this is doing?
-					// Excluding card from its own tags
-					continue;
-				}
-				decorator.AddTagTo(card);
-				if (taggedExit != "")
-				{
-					card.Connect(taggedExit, decorator,
-						nameof(decorator.OnZoneExit), new Array {card.Zone, card, "untag"});
-				}
-			}
-		}
-
-		protected void SetTargets(List<Card> cards)
-		{
-			Controller.DeclarePlay(new SetTargets(Card, cards));
-		}
-
-		protected void AutoTarget()
-		{
-			GameState.Paused = true;
-			Controller.DeclarePlay(new AutoTarget(Card));
-		}
+		
 	}
 	
 }
