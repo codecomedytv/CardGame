705ef830630f3a5a04ff144440b0e02b262db83c
CodeDarigan <darigan@protonmail.com>
2020-07-03 13:19:49 +0100

    Inline Attributes

--- Client/Room/Messenger.cs
+++ Client/Room/Messenger.cs
@@ -8,56 +8,35 @@ namespace CardGame.Client.Room
 {
     public class Messenger: Control
     {
-        [Signal]
-        public delegate void Retry();
-
-        [Signal]
-        public delegate void DeckLoaded();
+        [Signal] public delegate void DeckLoaded();
         
-        [Signal]
-        public delegate void Disqualified();
+        [Signal] public delegate void Disqualified();
 
-        [Signal]
-        public delegate void DrawQueued();
+        [Signal] public delegate void DrawQueued();
 
-        [Signal]
-        public delegate void DeployQueued();
+        [Signal] public delegate void DeployQueued();
 
-        [Signal]
-        public delegate void SetFaceDownQueued();
+        [Signal] public delegate void SetFaceDownQueued();
 
-        [Signal]
-        public delegate void ActivationQueued();
+        [Signal] public delegate void ActivationQueued();
 
-        [Signal]
-        public delegate void TriggerQueued();
+        [Signal] public delegate void TriggerQueued();
 
-        [Signal]
-        public delegate void CardStateSet();
+        [Signal] public delegate void CardStateSet();
 
-        [Signal]
-        public delegate void ValidTargetsSet();
+        [Signal] public delegate void ValidTargetsSet();
 
-        [Signal]
-        public delegate void ValidAttackTargetsSet();
+        [Signal] public delegate void ValidAttackTargetsSet();
 
-        [Signal]
-        public delegate void CardDestroyed();
+        [Signal] public delegate void CardDestroyed();
+        
+        [Signal] public delegate void UnitBattled();
 
-        [Signal]
-        public delegate void CardSentToGraveyard();
+        [Signal] public delegate void ExecutedEvents();
 
-        [Signal]
-        public delegate void UnitBattled();
+        [Signal] public delegate void DisconnectPlayer();
 
-        [Signal]
-        public delegate void ExecutedEvents();
-
-        [Signal]
-        public delegate void DisconnectPlayer();
-
-        [Signal]
-        public delegate void CardSentToZone();
+        [Signal] public delegate void CardSentToZone();
 
         private const int ServerId = 1;
         public int Id = 0;
@@ -66,62 +45,42 @@ namespace CardGame.Client.Room
         
         public void SetReady() => RpcId(ServerId, "SetReady", Id); // Complains about no network peer being assigned?
 
-        [Puppet]
-        public void Disqualify() => EmitSignal(nameof(Disqualified));
+        [Puppet] public void Disqualify() => EmitSignal(nameof(Disqualified));
+        [Puppet] public void ExecuteEvents(States stateAfterExecution) => EmitSignal(nameof(ExecutedEvents), stateAfterExecution);
         
-        [Puppet]
-        public void ExecuteEvents(States stateAfterExecution) => EmitSignal(nameof(ExecutedEvents), stateAfterExecution);
+        [Puppet] public void LoadDeck(Godot.Collections.Dictionary<int, SetCodes> deck) => EmitSignal(nameof(DeckLoaded), deck);
         
-        [Puppet]
-        public void LoadDeck(Godot.Collections.Dictionary<int, SetCodes> deck) => EmitSignal(nameof(DeckLoaded), deck);
+        [Puppet] public void QueueDraw(int id, SetCodes setCode) => EmitSignal(nameof(DrawQueued), id, setCode);
+
+        [Puppet] public void QueueDraw() =>EmitSignal(nameof(DrawQueued));
         
-        [Puppet]
-        public void QueueDraw(int id, SetCodes setCode) => EmitSignal(nameof(DrawQueued), id, setCode);
+        [Puppet] public void QueueDeploy(int id) => EmitSignal(nameof(DeployQueued), id);
 
-        [Puppet]
-        public void QueueDraw() =>EmitSignal(nameof(DrawQueued));
+        [Puppet] public void QueueDeploy(int id, SetCodes setCode) => EmitSignal(nameof(DeployQueued), id, setCode);
+
+        [Puppet] public void QueueSetFaceDown(int id) => EmitSignal(nameof(SetFaceDownQueued), id);
         
-        [Puppet]
-        public void QueueDeploy(int id) => EmitSignal(nameof(DeployQueued), id);
+        [Puppet] public void QueueSetFaceDown() => EmitSignal(nameof(SetFaceDownQueued));
 
-        [Puppet]
-        public void QueueDeploy(int id, SetCodes setCode) => EmitSignal(nameof(DeployQueued), id, setCode);
+        [Puppet] public void QueueActivation(int id, int positionInLink) => EmitSignal(nameof(ActivationQueued), id, positionInLink);
 
-        [Puppet]
-        public void QueueSetFaceDown(int id) => EmitSignal(nameof(SetFaceDownQueued), id);
+        [Puppet] public void QueueActivation(int id, SetCodes setCode, int positionInLink) => EmitSignal(nameof(ActivationQueued), id, setCode, positionInLink);
+
+        [Puppet] public void QueueTrigger(int id, int positionInLink) => EmitSignal(nameof(TriggerQueued), id, positionInLink);
         
-        [Puppet]
-        public void QueueSetFaceDown() => EmitSignal(nameof(SetFaceDownQueued));
+        [Puppet] public void SetCardState(int id, CardStates states) => EmitSignal(nameof(CardStateSet), id, states);
 
-        [Puppet]
-        public void QueueActivation(int id, int positionInLink) => EmitSignal(nameof(ActivationQueued), id, positionInLink);
+        [Puppet] public void SetValidTargets(int id, List<int> validTargets) => EmitSignal(nameof(ValidTargetsSet), id, validTargets);
 
-        [Puppet]
-        public void QueueActivation(int id, SetCodes setCode, int positionInLink) => EmitSignal(nameof(ActivationQueued), id, setCode, positionInLink);
+        [Puppet] public void SetValidAttackTargets(int id, List<int> validAttackTargets) => EmitSignal(nameof(ValidAttackTargetsSet), id, validAttackTargets); 
 
-        [Puppet]
-        public void QueueTrigger(int id, int positionInLink) => EmitSignal(nameof(TriggerQueued), id, positionInLink);
+        [Puppet] public void DestroyCard(int id) => EmitSignal(nameof(CardDestroyed), id);
         
-        [Puppet]
-        public void SetCardState(int id, CardStates states) => EmitSignal(nameof(CardStateSet), id, states);
+        [Puppet] public void QueueBattleUnit(int attackerId, int defenderId, bool isOpponentAttacking) => EmitSignal(nameof(UnitBattled), attackerId, defenderId, isOpponentAttacking);
 
-        [Puppet]
-        public void SetValidTargets(int id, List<int> validTargets) => EmitSignal(nameof(ValidTargetsSet), id, validTargets);
+        [Puppet] public void ForceDisconnected(int reason) => EmitSignal(nameof(DisconnectPlayer), reason);
 
-        [Puppet]
-        public void SetValidAttackTargets(int id, List<int> validAttackTargets) => EmitSignal(nameof(ValidAttackTargetsSet), id, validAttackTargets); 
-
-        [Puppet]
-        public void DestroyCard(int id) => EmitSignal(nameof(CardDestroyed), id);
-        
-        [Puppet]
-        public void QueueBattleUnit(int attackerId, int defenderId, bool isOpponentAttacking) => EmitSignal(nameof(UnitBattled), attackerId, defenderId, isOpponentAttacking);
-
-        [Puppet]
-        public void ForceDisconnected(int reason) => EmitSignal(nameof(DisconnectPlayer), reason);
-
-        [Puppet]
-        public void SentToZone(int cardId, ZoneIds zoneId) =>EmitSignal(nameof(CardSentToZone), cardId, zoneId);
+        [Puppet] public void SentToZone(int cardId, ZoneIds zoneId) =>EmitSignal(nameof(CardSentToZone), cardId, zoneId);
 
         public void Deploy(int cardId) => RpcId(ServerId, "Deploy", Id, cardId);
 
