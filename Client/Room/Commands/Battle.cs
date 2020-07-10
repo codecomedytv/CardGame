40adc438cc67f28cf8c2e313a54d1edb60c45cd6
Alex <darigan@protonmail.com>
2020-07-10 19:59:24 +0100

    Reduce Battle Command

--- Client/Room/Commands/Battle.cs
+++ Client/Room/Commands/Battle.cs
@@ -20,18 +20,18 @@ namespace CardGame.Client.Room.Commands
         {
             var attackerLocation = Attacker.RectPosition;
             var defenderLocation = Defender.RectPosition;
-            var attackerDestination = new Vector2(Attacker.RectPosition.x, Attacker.RectPosition.y - 50);
+            var attackerDestination = new Vector2(Attacker.RectPosition.x, Attacker.RectPosition.y - 150);
             var defenderDestination = new Vector2(Defender.RectPosition.x, Defender.RectPosition.y + 50);
             if (IsOpponent)
             {
                 // We were going the wrong way, so we reverse and compensate
-                attackerDestination.y += 100;
-                defenderDestination.y -= 100;
+                attackerDestination.y += 150;
+              //  defenderDestination.y -= 100;
             }
             QueueProperty(Attacker, "rect_position", attackerLocation, attackerDestination, 0.1F, 0.1F);
-            QueueProperty(Defender, "rect_position", defenderLocation, defenderDestination, 0.1F, 0.1F);
+            //QueueProperty(Defender, "rect_position", defenderLocation, defenderDestination, 0.1F, 0.1F);
             QueueProperty(Attacker, "rect_position", attackerDestination, attackerLocation, 0.1F, 0.2F);
-            QueueProperty(Defender, "rect_position", defenderDestination, defenderLocation, 0.1F, 0.2F);
+            //QueueProperty(Defender, "rect_position", defenderDestination, defenderLocation, 0.1F, 0.2F);
             QueueCallback(Attacker, 0.2F, nameof(Card.Deselect));
             QueueCallback(Defender, 0.2F, nameof(Card.Deselect));
             QueueCallback(Attacker, 0.2F, nameof(Card.StopAttacking));
