5144cc5baa0b952a0e52f8d5ece94b4c6c5a3f09
Alex <darigan@protonmail.com>
2020-07-15 18:07:37 +0100

    If To Ternary

--- Client/Room/Commands/Battle.cs
+++ Client/Room/Commands/Battle.cs
@@ -19,19 +19,10 @@ namespace CardGame.Client.Room.Commands
         protected override async Task<object[]> Execute()
         {
             var attackerLocation = Attacker.RectPosition;
-            var defenderLocation = Defender.RectPosition;
-            var attackerDestination = new Vector2(Attacker.RectPosition.x, Attacker.RectPosition.y - 150);
-            var defenderDestination = new Vector2(Defender.RectPosition.x, Defender.RectPosition.y + 50);
-            if (IsOpponent)
-            {
-                // We were going the wrong way, so we reverse and compensate
-                attackerDestination.y += 150;
-              //  defenderDestination.y -= 100;
-            }
+            var mod = IsOpponent ? 150 : -150;
+            var attackerDestination = new Vector2(Attacker.RectPosition.x, Attacker.RectPosition.y + mod);
             QueueProperty(Attacker, "rect_position", attackerLocation, attackerDestination, 0.1F, 0.1F);
-            //QueueProperty(Defender, "rect_position", defenderLocation, defenderDestination, 0.1F, 0.1F);
             QueueProperty(Attacker, "rect_position", attackerDestination, attackerLocation, 0.1F, 0.2F);
-            //QueueProperty(Defender, "rect_position", defenderDestination, defenderLocation, 0.1F, 0.2F);
             QueueCallback(Attacker, 0.2F, nameof(Card.Deselect));
             QueueCallback(Defender, 0.2F, nameof(Card.Deselect));
             QueueCallback(Attacker, 0.2F, nameof(Card.StopAttacking));
