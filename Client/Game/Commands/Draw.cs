7ba3fd3eba4947a866633b935703e6441bcb26e6
Alex <darigan@protonmail.com>
2020-09-09 14:39:56 +0100

    Strategy Getters Into Properties

--- Client/Game/Commands/Draw.cs
+++ Client/Game/Commands/Draw.cs
@@ -1,30 +1,30 @@
-﻿using CardGame.Client.Game.Cards;
+using CardGame.Client.Game.Cards;
 using Godot;
 
 namespace CardGame.Client.Game
 {
-    public class Draw: Command
-    {
-        private readonly Card Card;
+	public class Draw: Command
+	{
+		private readonly Card Card;
 
-        public Draw(Card card)
-        {
-            Card = card;
-        }
-        protected override void SetUp(Effects gfx)
-        {
-            Card.Visible = false;
-            Card.Controller.Deck.Add(Card);
-            var globalPosition = Card.Translation;
-            Card.Controller.Deck.Remove(Card);
-            Card.Controller.Hand.Add(Card);
-            var globalDestination = Card.Translation;
-            var rotation = new Vector3(-25, 180, 0);
-				
-            // Wrap In gfx Class
-            gfx.InterpolateProperty(Card, nameof(Card.Visible), false, true, 0.1F);
-            gfx.InterpolateProperty(Card, nameof(Card.Translation), globalPosition, globalDestination, 0.1F);
-            gfx.InterpolateProperty(Card, nameof(Card.RotationDegrees), Card.Rotation, rotation, 0.1F);
-        }
-    }
+		public Draw(Card card)
+		{
+			Card = card;
+		}
+		protected override void SetUp(Effects gfx)
+		{
+			Card.Visible = false;
+			Card.Controller.Deck.Add(Card);
+			var globalPosition = Card.Translation;
+			Card.Controller.Deck.Remove(Card);
+			Card.Controller.Hand.Add(Card);
+			var globalDestination = Card.Translation;
+			var rotation = new Vector3(-25, 180, 0);
+
+			// Wrap In gfx Class
+			gfx.InterpolateProperty(Card, nameof(Card.Visible), false, true, 0.1F);
+			gfx.InterpolateProperty(Card, nameof(Card.Translation), globalPosition, globalDestination, 0.1F);
+			gfx.InterpolateProperty(Card, nameof(Card.RotationDegrees), Card.Rotation, rotation, 0.1F);
+		}
+	}
 }
--- Client/Game/Commands/SetFaceDown.cs
+++ Client/Game/Commands/SetFaceDown.cs
@@ -1,28 +1,28 @@
-﻿using CardGame.Client.Game.Cards;
+using CardGame.Client.Game.Cards;
 using CardGame.Client.Game.Zones;
 using Godot;
 
 namespace CardGame.Client.Game
 {
-    public class SetFaceDown: Command
-    {
-        private readonly Card Card;
+	public class SetFaceDown: Command
+	{
+		private readonly Card Card;
 
-        public SetFaceDown(Card card)
-        {
-            Card = card;
-        }
-        protected override void SetUp(Effects gfx)
-        {
-            var origin = Card.Translation;
-            var destination = Card.Controller.Support.NextSlot() + new Vector3(0, 0, 0.05F);
+		public SetFaceDown(Card card)
+		{
+			Card = card;
+		}
+		protected override void SetUp(Effects gfx)
+		{
+			var origin = Card.Translation;
+			var destination = Card.Controller.Support.NextSlot() + new Vector3(0, 0, 0.05F);
 
-            Card.Controller.Hand.Remove(Card);
-            Card.Controller.Support.Add(Card);
+			Card.Controller.Hand.Remove(Card);
+			Card.Controller.Support.Add(Card);
 
-            gfx.InterpolateProperty(Card, nameof(Card.Translation), origin, destination, 0.3F);
-            gfx.InterpolateProperty(Card, nameof(Card.RotationDegrees), new Vector3(-25, 180, 0), new Vector3(0, 0, 0), 0.1F);
-            gfx.InterpolateCallback(Card.Controller.Hand, 0.2F, nameof(Hand.Sort));
-        }
-    }
+			gfx.InterpolateProperty(Card, nameof(Card.Translation), origin, destination, 0.3F);
+			gfx.InterpolateProperty(Card, nameof(Card.RotationDegrees), new Vector3(-25, 180, 0), new Vector3(0, 0, 0), 0.1F);
+			gfx.InterpolateCallback(Card.Controller.Hand, 0.2F, nameof(Hand.Sort));
+		}
+	}
 }
