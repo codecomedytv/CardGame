35e0e4a66cf403e4d2058559599c9f20422268a3
Alex <darigan@protonmail.com>
2020-09-24 10:52:03 +0100

    Cache Some Nodes

--- Client/Game/Cards/Card.cs
+++ Client/Game/Cards/Card.cs
@@ -13,7 +13,6 @@ namespace CardGame.Client.Game.Cards
 		public string Title;
 		public int Power;
 		public CardType CardType;
-		private CardFace _face = CardFace.FaceDown;
 		public string Effect;
 		public Texture Art;
 		public readonly Targets ValidTargets = new Targets();
@@ -24,6 +23,11 @@ namespace CardGame.Client.Game.Cards
 
 		public CardStates State => CardState.State;
 		private readonly CardState CardState;
+
+		private Area Area;
+		private Sprite3D AttackIcon;
+		private Sprite3D DefenseIcon;
+		private AnimationPlayer AnimationPlayer;
 
 		public bool IsHidden { get; set; } = true;
 		public Player OwningPlayer { get; set; }
@@ -36,6 +40,21 @@ namespace CardGame.Client.Game.Cards
 		public Card()
 		{
 			CardState = new CardState(this);
+		}
+		
+		public override void _Ready()
+		{
+			TargetReticule = GetNode<Sprite3D>("Target");
+			Area = GetNode<Area>("Area");
+			Area.Connect("mouse_entered", this, nameof(OnMouseEntered));
+			Area.Connect("mouse_exited", this, nameof(OnMouseExit));
+			AttackIcon = GetNode<Sprite3D>("Attacking");
+			DefenseIcon = GetNode<Sprite3D>("Defending");
+			AnimationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+			if (CardType == CardType.Unit)
+			{
+				ChangeAttack();
+			}
 		}
 
 		public void Update(CardStates state, IList<Card> targets, IList<Card> attackTargets)
@@ -51,48 +70,37 @@ namespace CardGame.Client.Game.Cards
 			GetNode<Sprite3D>("Face").Texture = Art;
 		}
 
-		public override void _Ready()
-		{
-			TargetReticule = GetNode<Sprite3D>("Target");
-			GetNode<Area>("Area").Connect("mouse_entered", this, nameof(OnMouseEntered));
-			GetNode<Area>("Area").Connect("mouse_exited", this, nameof(OnMouseExit));
-			if (CardType == CardType.Unit)
-			{
-				ChangeAttack();
-			}
-		}
-
+		
 		public void Target()
 		{
 			TargetReticule.Visible = true;
-			GetNode<AnimationPlayer>("AnimationPlayer").Play("Target");
+			AnimationPlayer.Play("Target");
 		}
 
 		public void StopTargeting()
 		{
 			TargetReticule.Visible = false;
-			GetNode<AnimationPlayer>("AnimationPlayer").Play("Playable");
-
+			AnimationPlayer.Play("Playable");
 		}
 
 		public void Attack()
 		{
-			GetNode<Sprite3D>("Attacking").Visible = true;
+			AttackIcon.Visible = true;
 		}
 
 		public void StopAttack()
 		{
-			GetNode<Sprite3D>("Attacking").Visible = false;
+			AttackIcon.Visible = false;
 		}
 
 		public void Defend()
 		{
-			GetNode<Sprite3D>("Defending").Visible = true;
+			DefenseIcon.Visible = true;
 		}
 
 		public void StopDefend()
 		{
-			GetNode<Sprite3D>("Defending").Visible = false;
+			DefenseIcon.Visible = false;
 		}
 		
 		private void ChangeAttack()
@@ -128,20 +136,10 @@ namespace CardGame.Client.Game.Cards
 				MouseOveredExit(this);
 			}
 		}
-
-		public void DisplayPower(int power)
-		{
-			throw new System.NotImplementedException();
-		}
-
+		
 		public void FlipFaceUp()
 		{
 			RotationDegrees = new Vector3(Rotation.x, 180, Rotation.z);
-		}
-
-		public void FlipFaceDown()
-		{
-			throw new System.NotImplementedException();
 		}
 	}
 }
